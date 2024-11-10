4183c5cdccd1b15a0da8208b699ba32b946e56d9
Volodymyr2121 <124473843+Volodymyr2121@users.noreply.github.com>
2024-11-11 00:36:15 +0100

    Update NotFoundPage.jsx

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,11 +1,9 @@
-import clsx from "clsx";
 import css from "./NotFoundPage.module.css";
 
 export default function NotFoundPage() {
-  const containerClass = clsx('mainContainer', css.errorContainer);
 
   return (
-    <div className={containerClass}>
+    <div className={css.errorContainer}>
       <h1>404 Page Not Found</h1>
       <p>Sorry, the page you are looking for does not exist.</p>
     </div>
